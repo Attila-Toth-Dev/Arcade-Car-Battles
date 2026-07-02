e497735059283dd4e964fd3145e2268c10c815e7
Attila Toth <attilatwork@gmail.com>
2026-07-02 23:33:31 +1000

    UPDATED: working on setting up basic controller + updated and added plugins into seperate plugins folder

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem;
 using FishNet.Object;
 
 using Inspector;
-using System;
 
 namespace Car
 {
@@ -12,14 +11,22 @@ namespace Car
     {
         [Header("References")]
         [SerializeField] private Rigidbody carRb;
-        [SerializeField] private Transform model;
-        [SerializeField] private Transform parent;
+        [SerializeField] private Transform carModel;
+        [SerializeField] private Transform carParent;
+        [SerializeField] private Transform carNormal;
 
-        [Header("Properties")]
-        [SerializeField] private float speed = 10.0f;
-        [SerializeField] private float steer = 10.0f;
-        [SerializeField] private float offset = 0.4f;
+        [Header("Car Properties")]
+        [SerializeField] private float topSpeed;
+        [SerializeField] private float acceleration;
+        [SerializeField] private float deceleration;
+        [SerializeField] private float steering;
 
+        [Header("Physics Properties")]
+        [SerializeField] private float gravity = 9.81f;
+        [SerializeField] private float colliderOffset = 0.5f;
+        [SerializeField] private float rayDistance = 1.1f;
+        [SerializeField] private LayerMask layerMask;
+ 
         [Header("Input References")]
         [SerializeField] private InputActionReference moveActionRef;
 
@@ -27,8 +34,16 @@ namespace Car
         [SerializeField, ReadOnly] private Vector2 moveInput;
         [SerializeField, ReadOnly] private bool isAccelerating;
         [SerializeField, ReadOnly] private bool isSteering;
-        [SerializeField, ReadOnly] private float currentSpeed;
-        [SerializeField, ReadOnly] private float currentSteer;
+        [SerializeField, ReadOnly] private bool isGrounded;
+
+        private float currentSpeed;
+        private float currentRotate;
+        private float speed;
+        private float rotate;
+
+        private Quaternion cachedRotation;
+
+        RaycastHit hitOn;
 
         public override void OnStartClient()
         {
@@ -38,6 +53,8 @@ namespace Car
                 return;
             
             moveActionRef.action.Enable();
+
+            cachedRotation = new Quaternion(carNormal.rotation.x, carNormal.rotation.y, carNormal.rotation.z, 0.0f);
         }
 
         public override void OnStopClient()
@@ -55,34 +72,94 @@ namespace Car
             if (!base.IsOwner)
                 return;
 
+            carParent.position = carRb.transform.position - new Vector3(0, colliderOffset, 0);
+
             InputHandler();
 
             CalculateAcceleration();
-            CalculateSteering();
+            CalculateSteering();   
         }
 
         private void FixedUpdate()
         {
             if (!base.IsOwner)
                 return;
+
+            GroundCheck();
+
+            ApplyAcceleration();
+            ApplySteering();
+            ApplyGravity();
+
+            RotateCarBody();
         }
 
         #region Update Functions
-        
+
         private void InputHandler()
         {
             moveInput = moveActionRef.action.ReadValue<Vector2>();
-            
-            isAccelerating = moveActionRef.action.ReadValue<Vector2>().y != 0.0f;
-            isSteering = moveActionRef.action.ReadValue<Vector2>().x != 0.0f;
+
+            isAccelerating = moveInput.y != 0.0f;
+            isSteering = moveInput.x != 0.0f;
         }
 
         private void CalculateAcceleration()
         {
+            if (isAccelerating)
+                speed = topSpeed * moveInput.y;
+
+            currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * acceleration);
         }
 
         private void CalculateSteering()
         {
+            if (isSteering)
+            {
+                int dir = moveInput.x > 0 ? 1 : -1;
+                float amount = Mathf.Abs(moveInput.x);
+
+                rotate = (steering * dir) * amount;
+            }
+            else
+                rotate = 0;
+
+            currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * deceleration);
+        }
+
+        #endregion
+
+        #region Fixed Update Functions
+
+        private void GroundCheck()
+        {
+            isGrounded = Physics.Raycast(transform.position, Vector3.down, out hitOn, rayDistance, layerMask);
+        }
+
+        private void ApplyAcceleration()
+        {
+            carRb.AddForce(carModel.transform.forward * (isGrounded ? currentSpeed : currentSpeed * 0.2f), ForceMode.Acceleration);
+        }
+
+        private void ApplySteering()
+        {
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.fixedDeltaTime * 5.0f);
+        }
+
+        private void ApplyGravity()
+        {
+            carRb.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+        }
+
+        private void RotateCarBody()
+        {
+            if(isGrounded)
+            {
+                carNormal.up = Vector3.Lerp(carNormal.up, hitOn.normal, Time.fixedDeltaTime * 8.0f);
+                carNormal.Rotate(0, transform.eulerAngles.y, 0);
+            }
+            else
+                carNormal.Rotate(cachedRotation.x, cachedRotation.y, cachedRotation.z);
         }
 
         #endregion
