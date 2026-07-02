35ce642989345b8763f24f89e8863d402aafdab7
Attila Toth <attilatwork@gmail.com>
2026-07-02 14:36:00 +1000

    ADDED: added basic input controls to the game + working now on the car moving

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 using FishNet.Object;
 
 using Inspector;
+using System;
 
 namespace Car
 {
@@ -11,13 +12,23 @@ namespace Car
     {
         [Header("References")]
         [SerializeField] private Rigidbody carRb;
+        [SerializeField] private Transform model;
+        [SerializeField] private Transform parent;
+
+        [Header("Properties")]
+        [SerializeField] private float speed = 10.0f;
+        [SerializeField] private float steer = 10.0f;
+        [SerializeField] private float offset = 0.4f;
 
         [Header("Input References")]
         [SerializeField] private InputActionReference moveActionRef;
 
         [Header("Debugging")]
         [SerializeField, ReadOnly] private Vector2 moveInput;
-        [SerializeField, ReadOnly] private bool isMoving;
+        [SerializeField, ReadOnly] private bool isAccelerating;
+        [SerializeField, ReadOnly] private bool isSteering;
+        [SerializeField, ReadOnly] private float currentSpeed;
+        [SerializeField, ReadOnly] private float currentSteer;
 
         public override void OnStartClient()
         {
@@ -45,31 +56,34 @@ namespace Car
                 return;
 
             InputHandler();
+
+            CalculateAcceleration();
+            CalculateSteering();
         }
 
         private void FixedUpdate()
         {
             if (!base.IsOwner)
                 return;
-
-            MoveVehicle();
         }
 
         #region Update Functions
         
         private void InputHandler()
         {
-
+            moveInput = moveActionRef.action.ReadValue<Vector2>();
+            
+            isAccelerating = moveActionRef.action.ReadValue<Vector2>().y != 0.0f;
+            isSteering = moveActionRef.action.ReadValue<Vector2>().x != 0.0f;
         }
 
-        #endregion
-
-        #region Fixed Update Functions
-        
-        private void MoveVehicle()
+        private void CalculateAcceleration()
         {
+        }
 
-        } 
+        private void CalculateSteering()
+        {
+        }
 
         #endregion
     }
