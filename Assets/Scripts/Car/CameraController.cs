317b5e267ad2ba00be90125783df7d911f49cad0
Attila Toth <attilatwork@gmail.com>
2026-07-03 21:27:11 +1000

    ADDED: added camera component to script and car prefab

--- Assets/Scripts/Car/CameraController.cs
+++ Assets/Scripts/Car/CameraController.cs
@@ -6,6 +6,56 @@ namespace Car
 {
     public class CameraController : NetworkBehaviour
     {
-                
+        [Header("References")]
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private Transform target;
+
+        [Header("Properties")]
+        [SerializeField] private float cameraHeight = 6.0f;
+        [SerializeField] private float cameraDistance = 6.0f;
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+
+            if(!base.IsOwner)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+
+            playerCamera.transform.LookAt(target);
+        }
+
+        public override void OnStopClient()
+        {
+            base.OnStopClient();
+
+            if (!base.IsOwner)
+                return;
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        private void LateUpdate()
+        {
+            if (!base.IsOwner)
+                return;
+
+            if(target == null)
+            {
+                Debug.LogWarning("Target is not assigned for the camera controller.", this);
+                return;
+            }
+
+            Vector3 camPos = new Vector3(target.position.x - cameraDistance, target.position.y + cameraHeight, target.position.z - cameraDistance);
+            
+            playerCamera.transform.position = camPos;
+            playerCamera.transform.LookAt(target);
+        }
     }
 }
