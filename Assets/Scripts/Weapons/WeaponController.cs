27efbc3fc11fe4e15eedbdc730330b6e83f206d6
Attila Toth <attilatwork@gmail.com>
2026-07-07 22:20:28 +1000

    Update WeaponController.cs

--- Assets/Scripts/Weapons/WeaponController.cs
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -76,8 +76,8 @@ namespace Weapons
             NetworkObject nob = Instantiate(currentWeapon, weaponAttachPoint.transform.position, Quaternion.identity);
             ServerManager.Spawn(nob, _conn);
 
-            //if(weaponAttachPoint.TryGetComponent(out NetworkBehaviour behaviour))
-            //    nob.SetParent(behaviour);
+            if(weaponAttachPoint.TryGetComponent(out NetworkBehaviour behaviour))
+                nob.SetParent(behaviour);
             
             //nob.transform.localPosition = Vector3.zero;
             //nob.transform.localRotation = Quaternion.identity;
