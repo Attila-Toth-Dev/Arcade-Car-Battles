f97b78b67676f75769141112d1e37f5234e40ade
Attila Toth <attilatwork@gmail.com>
2026-07-08 23:59:38 +1000

    Update CarController.cs

--- Assets/Scripts/Controllers/CarController.cs
+++ Assets/Scripts/Controllers/CarController.cs
@@ -106,24 +106,22 @@ namespace Controllers
 
             GroundCheck();
 
-            //MoveCar();
-            //RotateCar();
+            MoveCar();
+            RotateCar();
 
-            //RotateCarBody();
-            
-            ServerRpc_SendClientInput(moveInput);
+            RotateCarBody();
         }
 
         #region RPC Functions
 
-        [ServerRpc]
-        private void ServerRpc_SendClientInput(Vector2 _moveInput)
-        {
-            MoveCar(moveInput);
-            RotateCar(moveInput);
-
-            RotateCarBody();
-        }
+        //[ServerRpc]
+        //private void ServerRpc_SendClientInput(Vector2 _moveInput)
+        //{
+        //    MoveCar(moveInput);
+        //    RotateCar(moveInput);
+        //
+        //    RotateCarBody();
+        //}
 
         #endregion
 
@@ -156,18 +154,18 @@ namespace Controllers
             isGrounded = UnityEngine.Physics.Raycast(parent.transform.position, Vector3.down, out hitOn, rayDistance, layerMask);
         }
 
-        private void MoveCar(Vector2 _moveInput)
+        private void MoveCar()
         {
-            Vector3 moveDir = (camera.Forward * _moveInput.y) + (camera.Right * _moveInput.x);
+            Vector3 moveDir = (camera.Forward * moveInput.y) + (camera.Right * moveInput.x);
             rigidBody.AddForce(moveDir * currentSpeed, ForceMode.Acceleration);
         }
 
-        private void RotateCar(Vector2 _moveInput)
+        private void RotateCar()
         {
             if (moveInput.magnitude == 0)
                 return;
 
-            rotationDirection = (camera.Forward * _moveInput.y) + (camera.Right * _moveInput.x);
+            rotationDirection = (camera.Forward * moveInput.y) + (camera.Right * moveInput.x);
             stickAngle = Mathf.Atan2(rotationDirection.x, rotationDirection.z) * Mathf.Rad2Deg;
 
             targetAngle = Mathf.SmoothDampAngle(parent.eulerAngles.y, stickAngle, ref turnVelocity, Time.fixedDeltaTime * setSteer);
