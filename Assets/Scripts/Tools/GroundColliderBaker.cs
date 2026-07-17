e0c0072aae4d0e1c4ae8c6194226adf315040691
Attila Toth <attilatwork@gmail.com>
2026-07-17 14:15:59 +1000

    UPDATED: updated collisions per tile + added new mesh baker tool for collisions

---
+++ Assets/Scripts/Tools/GroundColliderBaker.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+namespace Tools
+{
+    public class GroundColliderBaker
+    {
+        [MenuItem("Tools/Bake Ground Collision Mesh")]
+        static void BakeGroundCollision()
+        {
+
+        }
+    }
+}
